48ea58c6fc8a90866ad4b8266e0a7c832c109503
Fran Fiorani <Fran.Fiorani@softwire.com>
2024-07-02 14:07:40 +0100

    Refactored the previous part so that the fizzprint would only have to print, by adding a words vector implementation. Added reversion when divisible by 17

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,7 @@
 // This is our main function
 
 function fizzprint(message, i) {
-    if (i % 11 == 0) {
-        console.log("Bong")
-    } else if (message.length != 0) {
+    if (message.length != 0) {
         console.log(message);
     } else {
         console.log(i);
@@ -11,25 +9,46 @@ function fizzprint(message, i) {
 }
 
 function fizzbuzz() {
-    let n = 105;
+    let n = 255;
 
     for (let i = 1; i <= n; i++) {
-        let message = ""
+        var words = []
+
         if (i % 3 == 0) {
-            message += "Fizz";
+            words.push("Fizz");
         }
 
         if (i % 5 == 0) {
-            message += "Buzz";
+            words.push("Buzz");
         }
 
         if (i % 7 == 0) {
-            message += "Bang";
+            words.push("Bang");
         }
 
+        if (i % 11 == 0) {
+            words = ["Bong"];
+        }
+
+        if (i % 13 == 0) {
+            if (words.length == 0) {
+                words.push("Fezz");
+            } else {
+                let j = 0;
+                while (j < words.length && words[j][0] != 'B') { j++; }
+                words.splice(j, 0, "Fezz");
+            }
+        }
+
+        if (i % 17 == 0) {
+            words.reverse();
+        }
+
+        let message = words.join("");
         fizzprint(message, i);
     }
 }
+
 
 // Now, we run the main function:
 fizzbuzz();
